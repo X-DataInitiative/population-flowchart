6a76fe078ce38f04504e37c24edd4a7ae91c20dd
Daniel de Paula e Silva <danielpes91@gmail.com>
2018-02-11 20:36:30 +0100

    Modify app.js to query elements on window.onload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 App = (function() {
 
-  var $jsonFile = document.getElementById('json-file')
-  var $jsonField = document.getElementById('json-field')
-  var $okButton = document.getElementById('ok-button')
-  var $fileNameBox = document.getElementById('file-name-box')
-  var $graphContainer = document.getElementById('graph')
+  var $jsonFile
+  var $jsonField
+  var $okButton
+  var $fileNameBox
+  var $graphContainer
+
+  function start() {
+    window.onload = function() {
+      initElements()
+      bindEvents()
+    }
+  }
+
+  function initElements() {
+    $jsonFile = document.getElementById('json-file')
+    $jsonField = document.getElementById('json-field')
+    $okButton = document.getElementById('ok-button')
+    $fileNameBox = document.getElementById('file-name-box')
+    $graphContainer = document.getElementById('graph')
+  }
+
+  function bindEvents() {
+    $jsonFile.onchange = handleFileChange
+    $okButton.onclick = handleButtonClick
+  }
 
   function handleFileChange() {
     if($jsonFile.files.length > 0) {
@@ -22,15 +42,6 @@ App = (function() {
       var parsedData = Parser.fromText(jsonText)
       Graph.draw($graphContainer, parsedData)
     }
-  }
-
-  function bindEvents() {
-    $jsonFile.onchange = handleFileChange
-    $okButton.onclick = handleButtonClick
-  }
-
-  function start() {
-    bindEvents()
   }
 
   return {
